d4ea0a6e8eee2b8addc6a3d27703e9aedb1f8813
kauppine <kauppine@users.noreply.github.com>
2019-11-09 18:38:50 +0200

    Improved support for /64 and /56

--- option-6rd.sh
+++ option-6rd.sh
@@ -83,9 +83,19 @@ log_6rd() {
 
 	logger -t option-6rd "6RD parameters: 6rd-prefix ${srd_prefix}/${srd_prefixlen} 6rd-relay_prefix ${srd_relayprefix}/${srd_masklen} br ${srd_braddr}"
 	delagated_prefix=`ipv6calc -q --action 6rd_local_prefix --6rd_prefix ${srd_prefix}/${srd_prefixlen} --6rd_relay_prefix ${srd_relayprefix}/${srd_masklen} $WANIP4`
-	ifname_ip6addr="$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[1]}')1/$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[2]}')"
-	lan_ip6addr="$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[1]}')1/64" # Need to change if using subnet (if Delagated prefix < 64)
-	lan_ip6net="$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[1]}')/64" # Need to change if using subnet (if Delagated prefix < 64)
+	prefix_len=$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[2]}')
+	ifname_ip6addr="$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[1]}')1/128"
+	if ((prefix_len == 56)); then
+		lan_ip6addr="$(echo "$delagated_prefix" | awk -F : '{printf "%s:%s:%s:%.2s01::1/64\n", $1, $2, $3, $4}')"
+		lan_ip6addr="$(echo "$delagated_prefix" | awk -F : '{printf "%s:%s:%s:%.2s01::/64\n", $1, $2, $3, $4}')"
+	elif ((prefix_len == 64)); then
+		lan_ip6addr="$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[1]}')1/64"
+		lan_ip6net="$(echo "$delagated_prefix" | awk '{split($0,a,"/"); print a[1]}')/64"
+	else
+		logger -p daemon.error -t option-6rd "Unsupported prefix length $prefix_len"
+		return
+	fi
+
 
 
 	if [ -f /config/telia-6rd-cleanup ]; then
